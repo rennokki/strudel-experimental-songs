f078f040ccda5eba7a3fbd84bcbda82aac447657
rennokki <alex@thecodefather.co>
2025-11-26 11:48:51 +0200

    Add strum functions and G chord example

---
+++ utils.js
@@ -0,0 +1,13 @@
+function strum(pattern = [0,0,0,0,0,0]) {
+  // TODO
+}
+
+function strum(chord = "e2") {
+  // TODO
+}
+
+// Example of a G chord
+$: sound("gm_acoustic_guitar_steel")
+  // .note("[e2,a3,d3,g3,b4,e4]")
+  .note("[e2,a3,d3,g3,b4,e4]")
+  .trans("[3,2,0,0,0,3]")
